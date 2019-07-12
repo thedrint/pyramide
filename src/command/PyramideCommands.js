ed349b8a0231eb99b9c3ae9bc5889e9c4c919cc0
The Drint <thedrint@yandex.ru>
2019-07-12 09:30:42 +0300

    Small Refactoring for PyramideCommands

--- src/command/PyramideCommands.js
+++ src/command/PyramideCommands.js
@@ -12,26 +12,22 @@ export class doGetCardFromDealerDeck extends Command {
 			let newCard = dealer.deck.pop();
 			dealer.slot.add(newCard);
 			gui.animations.add(gui.animation('AnimationToSlot', newCard.model, 200))
-			this.ended();
-			return true;
+			this.ended(); return true;
 		}
-		else {
-			if( logic.currentRewind > logic.maxRewinds-1 ) {
-				gui.showModal(logic.game.i18n.t('You cannot rewind anymore!'));
-				this.ended();
-				return false;
-			}
-			// Set all cards from Dslot to Ddeck
-			while( dealer.slot.length ) {
-				let card = dealer.slot.pop();
-				card.model.showShirt();
-				dealer.deck.add(card);
-				dealer.model.Deck.addChild(card.model);
-			}
-			logic.currentRewind++;
-			this.ended();
-			return true;
+		if( logic.currentRewind > logic.maxRewinds-1 ) {
+			gui.showModal(logic.game.i18n.t('You cannot rewind anymore!'));
+			this.ended(); return false;
 		}
+		// Set all cards from Dslot to Ddeck
+		//TODO: Animation for rewind
+		while( dealer.slot.length ) {
+			let card = dealer.slot.pop();
+			card.model.showShirt();
+			dealer.deck.add(card);
+			dealer.model.Deck.addChild(card.model);
+		}
+		logic.currentRewind++;
+		this.ended(); return true;
 	}
 }
 
@@ -43,82 +39,60 @@ export class undoGetCardFromDealerDeck extends Command {
 			let lastSlotCard = dealer.slot.pop();
 			dealer.deck.add(lastSlotCard);
 			gui.animations.add(gui.animation('AnimationToDeck', lastSlotCard.model, 200))
-			this.ended();
-			return true;
+			this.ended(); return true;
 		}
 		// Slot empty, check if dealer deck was rewinded
-		else {
-			// DDeck is not rewinded yet
-			if( logic.currentRewind === 0 ) {
-				// console.log(`You unrewind deck to initial position`);
-				this.ended();
-				return false;
-			}
-
-			// DDeck was rewinded, undo this
-			while( dealer.deck.length ) {
-				let card = dealer.deck.pop();
-				dealer.slot.add(card);
-				dealer.model.Slot.addChild(card.model);
-				card.model.showFace();
-			}
-			logic.currentRewind--;
-			this.ended();
-			return true;
+		// DDeck is not rewinded yet
+		if( logic.currentRewind === 0 ) { this.ended(); return true; }
+		// DDeck was rewinded, undo this
+		//TODO: Animation for this
+		while( dealer.deck.length ) {
+			let card = dealer.deck.pop();
+			dealer.slot.add(card);
+			dealer.model.Slot.addChild(card.model);
+			card.model.showFace();
 		}
+		logic.currentRewind--;
+		this.ended();
+		return true;
 	}
 }
 
 export class doDropCards extends Command {
 	run () {
 		let logic = this.rec;
-		let {scene:gui, dealer, drop, field} = logic;
+		let {scene:gui, dealer, drop, field, scoreboard, game} = logic;
 		let arrayOfCards = this.params;
 		for( let card of arrayOfCards ) {
 			if( card.from == 'field' ) field.getCell(card.row, card.index).removeCard();
 			else dealer.slot.pop();
-			// Add card to drop
 			drop.cards.add(card);
 			gui.scoreboard.scores += card.score;
 			gui.animations.add(gui.animation('AnimationDrop', card.model, 300))
 		}
-
 		// Check game win
-		if( logic.isFieldDeckEmpty() ) {
-			gui.showModal(logic.game.i18n.t('You win a game!', {scores: logic.scoreboard.scores}));
-			//TODO: Win animation
-		}
-		this.ended();
-		return true;
+		//TODO: Win animation
+		if( logic.isFieldDeckEmpty() ) gui.showModal(game.i18n.t('You win a game!', {scores: scoreboard.scores}));
+		this.ended(); return true;
 	}
 }
 
 export class undoDropCards extends Command {
 	run () {
 		let logic = this.rec;
+		if( !logic.drop.cards.length ) { this.failed(); return false; }
 		let {scene:gui, dealer, drop, field} = logic;
 		let cards = this.params;
-		if( !logic.drop.cards.length ) {
-			this.failed();
-			return false;
-		}
 		// Remove cards from drop
 		let indexes = [];
 		for( let droppedCard of cards ) {
-			let index = drop.cards.findIndex(card => {
-				return (card.name == droppedCard.name);
-			});
+			let index = drop.cards.findIndex(card => { return (card.name == droppedCard.name) });
 			let card = drop.cards.delete(index);
 			if( card.from == 'field' ) field.getCell(card.row, card.index).card = card;
 			else dealer.slot.add(card);
-		}
-
-		for( let card of cards ) {
 			gui.scoreboard.scores -= card.score;
 			gui.animations.add(gui.animation('AnimationUnDrop', card.model, 300));
 		}
-
-		this.ended();
-		return true;
+		this.ended(); return true;
 	}
 }
